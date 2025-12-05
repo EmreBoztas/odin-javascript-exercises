2a2b734b4467daae0662609ae6a56b418cf811dc
emreboztas <emreb2619@gmail.com>
2025-12-05 22:30:47 +0100

    Complete exercise 9 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum) || firstNum < 0 || secondNum < 0) 
+    {
+      return "ERROR";
+    }
+    if (firstNum < secondNum) 
+    {
+      return ((firstNum + secondNum) * (secondNum - firstNum + 1) / 2);
+    }
+    else if (firstNum > secondNum) 
+    {
+      return ((firstNum + secondNum) * (firstNum - secondNum + 1) / 2);
+    }
+    else 
+    {
+      return "ERROR";
+    }
 };
+
+console.log(sumAll(2, 4)); // 9
 
 // Do not edit below this line
 module.exports = sumAll;
