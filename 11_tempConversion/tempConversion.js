66bc3c13244310b0ec97c6a221eb28673a18a016
emreboztas <emreb2619@gmail.com>
2025-12-06 20:40:25 +0100

    Complete exercise 11 temp conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function( fahrenheit ) {
+  let result = Math.round(((fahrenheit - 32) * 5 / 9) * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function( celsius) {
+  let result = Math.round(((celsius * 9 / 5) + 32) * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
